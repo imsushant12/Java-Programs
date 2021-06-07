37a5bd27e825d676af0c20a379ef264626f8eec6
imsushant12 <sushantgaurav57@gmail.com>
2021-06-08 00:07:51 +0530

    first commit

---
+++ Student.java
@@ -0,0 +1,25 @@
+//package secondPackage;
+
+public class Student 
+{
+    private int roll;
+    private String name;
+
+    public void setName(String n)
+    {
+        name = n;
+    }
+    public void setRoll(int r)
+    {
+        roll = r;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    public int getRoll()
+    {
+        return roll;
+    }
+}
