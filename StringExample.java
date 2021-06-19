fd204d12169fd5ed36e017dce4922d4384dc2d65
imsushant12 <sushantgaurav57@gmail.com>
2021-06-19 18:23:00 +0530

    first commit

---
+++ StringExample.java
@@ -0,0 +1,10 @@
+public class StringExample 
+{
+    public static void main(String[] args) 
+    {
+        String s1 = "Hello";
+        System.out.println(s1.toUpperCase());
+        System.out.println(s1.toLowerCase());
+    }
+
+}
