59048ccc20a398000ba8cf22f208d680cc11fa1c
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-02-02 18:48:49 +0530

    first commit

---
+++ Pattern_Butterfly.java
@@ -0,0 +1,50 @@
+// *       *
+// **     **
+// ***   ***
+// **** ****
+// **** ****
+// ***   ***
+// **     **
+// *       *
+
+import java.util.Scanner;
+
+public class Pattern_Butterfly {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int row = sc.nextInt();
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 1; j <= 2 * (row - i); j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = row; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 1; j <= 2 * (row - i); j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
