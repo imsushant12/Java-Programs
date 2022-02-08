c12cfea8102dee505fbd464190cf9616e13269e1
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-02-08 19:44:18 +0530

    first commit

---
+++ Pattern_Diamond.java
@@ -0,0 +1,43 @@
+//    *
+//   ***
+//  *****
+// *******
+// *******
+//  *****
+//   ***
+//    *
+
+import java.util.Scanner;
+
+public class Pattern_Diamond {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int row = sc.nextInt();
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < (row - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (2 * i) + 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = row - 1; i >= 0; i--) {
+            for (int j = 0; j < (row - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (2 * i) + 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
